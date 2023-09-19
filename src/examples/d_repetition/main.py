97be956b861ed962937a77a8fac15ef850587279
JZarz <142853226+JZarz@users.noreply.github.com>
2023-09-19 00:21:32 +0000

    loops sum of square + strings lesson

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -1,3 +1,5 @@
 import repetition
 #repetition.display_numbers(2)
-repetition.prompt_user()
+#repetition.prompt_user()
+#repetition.for_intro_loop()
+repetition.for_intro_loop_strings()
--- tests/examples/d_repetition/tests_repetition.py
+++ tests/examples/d_repetition/tests_repetition.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.d_repetition.repetition import sum_of_square, test_config
+from src.examples.d_repetition.repetition import sum_of_square, sum_of_squares, test_config
 
 class Test_Config(unittest.TestCase):
 
@@ -12,5 +12,10 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(sum_of_square(4), 30)
         self.assertEqual(sum_of_square(5), 55)
         self.assertEqual(sum_of_square(-1), 0)
+        
+    def test_for_sum_of_squares(self):
+        self.assertEqual(sum_of_squares(3), 14)
+        self.assertEqual(sum_of_squares(4), 30)
+        self.assertEqual(sum_of_squares(5), 55)
 
 
