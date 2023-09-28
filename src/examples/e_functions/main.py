910d1452db88eead1177a6c377599767eb735870
JZarz <142853226+JZarz@users.noreply.github.com>
2023-09-28 00:58:00 +0000

    value return functions + memory stack lesson

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,5 +1,9 @@
 #main program
 
-import void_functions
-void_functions.display()
-void_functions.say_hello("jenifer")
+import value_return_functions
+#import void_functions
+#void_functions.display()
+#void_functions.say_hello("jenifer")
+
+value_return_functions.global_variable()
+value_return_functions.global_variable_2()
--- src/examples/e_functions/value_return_functions.py
+++ src/examples/e_functions/value_return_functions.py
@@ -1,2 +1,34 @@
+import random
+
+
 def test_config():
     return True
+
+def is_num_odd(num):
+    return num % 2 == 1
+
+def local_function_variable(val0):
+    val = 100
+    val2 = 10
+
+val3 = 5
+
+def global_variable_w_param(val3):
+    val3 = 10
+
+def global_variable():
+    print(val3)
+    
+def global_variable_1():
+    global val3
+    val3 = 10
+    print (val3)
+
+def global_variable_2():
+    print (val3)
+
+def get_random_number(min, max):
+    return random.randint (min, max)
+
+def return_two_functions_values(num1, num2):
+    return num1, num2
