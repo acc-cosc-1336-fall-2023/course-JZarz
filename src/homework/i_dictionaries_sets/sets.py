4b697c6045f539e9d7c131cd775c584b3eaf43fe
JZarz <142853226+JZarz@users.noreply.github.com>
2023-10-21 21:38:31 +0000

    hw 6 loops in list

--- src/homework/i_dictionaries_sets/sets.py
+++ src/homework/i_dictionaries_sets/sets.py
@@ -1,1 +1,63 @@
-#
+def get_p_distance(list1, list2):
+    if len(list1) != len(list2):
+        print("length of strings aren't equal")
+    else:
+        distance = 0
+        for i in range (0, len(list1)):
+            if list1[i] !=list2[i]:
+                distance += 1
+        return distance
+    
+def get_p_distance_matrix(list1):
+    matrix = []
+    i = 0
+    j = 0
+    for i in range(0, (len(list1))):
+        l1 = []
+        for j in range (0,len(list1)):
+            distance = get_p_distance(list1[i],list1[j])
+            fasta = distance / 10
+            l1.append(fasta)
+        i += 1
+        matrix.append(l1)
+    return matrix
+
+def menu():
+    choice = input("Select Option \n 1. Get P Distance Matrix:\n 2. Exit: ")
+    option(choice)
+
+def option(choice):
+    while choice != '2':
+        if choice == '1' :
+            option1()
+        if choice != '2' and choice != '1':
+            choice = input("please select 1. or 2.: ")
+
+    print("Program will Exit")
+    
+
+def option1():
+    list_amt = int(input("how many str would you like to compare\n"))
+    list1 = []
+    i = 0
+    for i in range (0, list_amt):
+        inp_list = input("enter dna str: ")
+        list1.append(inp_list)
+    i += 1
+
+
+    matrix = get_p_distance_matrix(list1)
+    print(matrix)
+    cont()
+
+def cont():
+    answer = input("Would you like to get another matrix? \n Type 'Y' for Yes\n Type 'N' to Exit: ")
+    if answer.upper() != "N":
+        while answer.upper() != "N" or answer.upper() != "Y":
+            option1()
+            while answer.upper() != "N" and answer.upper() != "Y":
+                answer = input("Please select 'Y' or 'N': ")
+    else:
+        print("Program will exit")
+        exit()
+
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,11 @@
-#
+import unittest
+from src.homework.i_dictionaries_sets.sets import get_p_distance, get_p_distance_matrix
+
+class test_config(unittest.TestCase):
+    def test_get_p_distance(self):
+        self.assertEqual(get_p_distance(['T','T','T','C','C','A','T','T','T','A'],  ['G','A','T','T','C','A','T','T','T','C']), 4)
+
+    def  test_get_p_distance_matrix(self):
+        self.assertEqual(get_p_distance_matrix(
+[['T','T','T','C','C','A','T','T','T','A'],['G','A','T','T','C','A','T','T','T','C'],['T','T','T','C','C','A','T','T','T','T'],['G','T','T','C','C','A','T','T','T','A']]), 
+[[0.0, 0.4, 0.1, 0.1],[0.4, 0.0, 0.4, 0.3],[0.1, 0.4, 0.0, 0.2],[0.1, 0.3, 0.2, 0.0]])
