02a1a587334e6ba2d18affe5cae382e3ede8c7e7
JZarz <142853226+JZarz@users.noreply.github.com>
2023-10-28 16:18:41 +0000

    text change

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -3,7 +3,7 @@ import sets, dictionary
 inventory = {}
 
 def menu():
-    choice = input(" 1.Add or update Inventory\n 2. Remove Inventory\n 3.Exit\n Choose option: ")
+    choice = input(" 1. Add or update Inventory\n 2. Remove Inventory\n 3. Exit\n Choose option: ")
     options(choice)
 
 def options(choice):
@@ -16,24 +16,24 @@ def options(choice):
         else:
             print('Invalid option')
     else:
-        print('program will exit')
+        print('Program Will Exit')
 
 def option1():
-    number = int(input('how many items would you like to add? '))
+    number = int(input('how many items would you like to add and/ or revise? '))
     for i in range(0, number):
         
-        widget_name = input('Name of item you want to add: ')
+        widget_name = input('Name of item you want to add/revise: ')
         quantity = input('Quantity: ')
         dictionary.add_inventory(widget_name,quantity, inventory)
         i += 1
     
-    print('inventory:', inventory)
+    print('Inventory:', inventory)
     cont()
 
 def option2():
     widget_name = input('Name of item you want to remove: ')
     dictionary.remove_inventory(widget_name,inventory)
-    print(inventory)
+    print('Inventory:', inventory)
     cont()
 
 def cont():
@@ -42,6 +42,7 @@ def cont():
     if answer.upper() == 'Y':
         menu()
     elif answer.upper() == 'N':
+        print('Final Inventory:', inventory)
         print('Program Exited')
     else:
         print('Invalid Option')
