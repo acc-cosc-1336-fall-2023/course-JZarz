3ea36ecc6eb1526a1905673d69213298483589c7
JZarz <142853226+JZarz@users.noreply.github.com>
2023-10-31 00:15:01 +0000

    txt files read + write + append + user input

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -1,1 +1,43 @@
-#
+def write_to_file(file_name):
+    out_file = open(file_name, 'w')
+    #write to file
+    out_file.write('John Locke\n')
+    out_file.write('David Hume\n')
+    out_file.write('Edmund Burke\n')
+
+    #close the file
+    out_file.close()
+    
+def read_from_file(file_name):
+    in_file = open(file_name,'r')
+
+    file_contents = in_file.read()
+
+    #close file
+    in_file.close()
+    print(file_contents)
+
+def read_each_line_from_file(file_name):
+    in_file = open(file_name,'r')
+    line1 = in_file.readline()
+    line2 = in_file.readline()
+    line3 = in_file.readline()
+
+    in_file.close()
+
+    print(line1)
+    print(line2)
+    print(line3)
+    #if you dont want space you have to strip line space .rstrip('\n')
+#cant use testcase because we dont want test to create new info
+def write_names_to_file(file_name, file_mode):
+    out_file = open(file_name,file_mode)
+
+    user_choice = 'y'
+
+    while(user_choice.lower() == 'y'):
+        name = input('enter friend: ')
+        out_file.write(name +'\n')
+        user_choice = input('type y to continue...')
+
+    out_file.close()
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,1 +1,12 @@
 #main program
+import files
+#files.write_to_file('philsophers.txt')
+
+#ls to check files created
+#vi file prints/reads txt
+
+#files.read_from_file('philsophers.txt')
+
+#files.read_each_line_from_file('philsophers.txt')
+
+files.write_names_to_file('names.txt', 'a')
