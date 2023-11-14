031229b000e25f0ba69c6898ce526c96d12dd6b5
JZarz <142853226+JZarz@users.noreply.github.com>
2023-11-14 00:38:36 +0000

    import private random int for balance

--- src/examples/j_classes/bank_account.py
+++ src/examples/j_classes/bank_account.py
@@ -1,8 +1,13 @@
+import random
 class BankAccount:
     __balance = 0 #keeps it private from other classes
 
     def __init__(self, balance): #constructio -- initialze class data/var
-        self.__balance = balance
+        
+        if(balance>= 0 ):
+            self.__balance = balance
+        else:
+            self.__get_balance_from_db()#this is a private function and not accepted in other classes
         
     def get_balance(self):#public other classes can access
         return self.__balance
@@ -15,4 +20,6 @@ class BankAccount:
     def withdraw(self, amount):
         if (amount > 0 and amount <= self.__balance):
             self.__balance -= amount
-            
+
+    def __get_balance_from_db(self):
+            self.__balance = random.randint(0, 10000)
